bf3bc05425415e5f5fa7ef138e9dc1677eeeed26
belcattaneo <belcattaneo@gmail.com>
2018-04-03 11:19:47 -0300

    added view result with query params

--- fem/views.py
+++ fem/views.py
@@ -3,3 +3,21 @@ from django.http import HttpResponse
 
 def index(request):
     return render_to_response('fem/index.html')
+
+def result(request):
+    size = request.GET['size']
+    top = request.GET['top']
+    right = request.GET['right']
+    bottom = request.GET['bottom']
+    left = request.GET['left']
+    context =  {'data': 
+        {
+            'size' : size,
+            'top' : top,
+            'right' : right,
+            'bottom' : bottom,
+            'left' : left
+        }
+    }
+    print(context)
+    return render(request, 'fem/index.html', context)
