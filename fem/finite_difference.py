Added error calculation. Fixed 0 right temperature bug.

fem/finite_difference.py
@@ -7,6 +7,7 @@ def diferencias_finitas(temperatures, source, size):
     constant_matrix = build_constant_matrix(matrix, source)
     coeficient_matrix = build_coeficient_matrix(constant_matrix, matrix)
     results = get_results(constant_matrix, coeficient_matrix)
+    print(results)
     final_matrix = build_final_matrix(matrix, results)
 
     return final_matrix
@@ -79,24 +80,29 @@ def build_coeficient_matrix(constant_matrix, matrix):
     return np.array(matrix_coeficients)
    
 def get_results(constant_matrix, coeficient_matrix):
-    return np.linalg.inv(coeficient_matrix).dot(constant_matrix)
+    results = np.linalg.solve(np.array(coeficient_matrix), np.array(constant_matrix))
+    
+    return np.linalg.solve(np.array(coeficient_matrix), np.array(constant_matrix))
 
 def build_final_matrix(matrix, results):
     results_index = 0
     final_matrix = np.copy(matrix)
     size = np.array(results).size
 
-    x = 0
-    for column in final_matrix:
-        y = 0
-        for row in column:
-            if results_index < size:
-                    if (final_matrix[x,y] == 0 and x > 0 and y > 0 and x < size-1 and y < size-1):
+    if size == 1:
+        final_matrix[1,1] = results[0]
+    else:
+        x = 0
+        for column in final_matrix:
+            y = 0
+            for row in column:
+                if results_index < size:
+                    if (final_matrix[x,y] == 0 and x > 0 and y > 0 and x < np.array(column).size-1 and y < np.array(column).size-1):
                         final_matrix[x, y] = results[results_index]
                         results_index += 1
                     
                     y += 1
-        x += 1
+            x += 1
 
 
     return final_matrix

fem/galerkin.py
@@ -1,5 +1,4 @@
 import numpy as np
-import functools
 
 
 def galerkin(size, temperatures, source):
@@ -120,7 +119,8 @@ def build_final_matrix(matrix, results):
     results_index = 0
     final_matrix = np.copy(matrix)
     size = np.array(results).size
-    print(size)
+
+    
     if size == 1:
         final_matrix[1,1] = results[0]
     else:
@@ -128,8 +128,9 @@ def build_final_matrix(matrix, results):
         for column in final_matrix:
             y = 0
             for row in column:
+                print(np.array(column).size)
                 if results_index < size:
-                    if (final_matrix[x,y] == 0 and x > 0 and y > 0 and x < size-1 and y < size-1):
+                    if (final_matrix[x,y] == 0 and x > 0 and y > 0 and x < np.array(column).size-1 and y < np.array(column).size-1):
                         final_matrix[x, y] = results[results_index]
                         results_index += 1
                     