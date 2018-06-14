a1fb674fcf243ab84d865c0cab589191ad60fabf
belcattaneo <belcattaneo@gmail.com>
2018-06-14 20:00:55 -0300

    No se tienen en cuenta los valores en los bordes.

--- fem/resolucion_analitica.py
+++ fem/resolucion_analitica.py
@@ -5,15 +5,11 @@ from scipy import integrate
 
 def resolucion_analitica(size, temperatures, source):
     method_temperatures = get_temperatures_I(temperatures, size)
-    print(method_temperatures.size)
     method_source = get_temperatures_II(size, source)
-    print(method_source.size)
 
     results = np.array(method_temperatures) + np.array(method_source)
 
-    temp_matrix = create_matrix_from_inputs(temperatures, size)
-
-    final_matrix = build_final_matrix(temp_matrix, results)
+    final_matrix = build_final_matrix(results)
 
     return final_matrix
 
@@ -40,8 +36,8 @@ def sumation(temperatures, x, y):
 
 def get_temperatures_I(temperatures, size):
     
-    step = 1 / (size - 1)
-    interval = np.arange(step, 1, step)[0:(size-2)]
+    step = 1 / (size)
+    interval = np.arange(step/2, 1, step)[0:(size)]
     results_matrix = []
     for y in interval:
         row = []
@@ -54,8 +50,8 @@ def get_temperatures_I(temperatures, size):
     
 
 def get_temperatures_II(size, source):
-    step = 1 / (size - 1)
-    interval = np.arange(-1/2+step, 1/2, step)[0:(size-2)]
+    step = 1 / (size)
+    interval = np.arange((-1/2+step/2), 1/2, step)[0:(size)]
     pi = math.pi
     a = 1/2
     
@@ -72,27 +68,12 @@ def get_temperatures_II(size, source):
             row.append(source*(a**2)*t)
         
         results_matrix.insert(0, row)   
-    
-    
     return np.array(results_matrix).flatten()
 
-
-def create_matrix_from_inputs(temperatures, size):
-    matrix = np.zeros((size, size), dtype=np.int)
-    x = 0
-    for column in matrix:
-        y = 0
-        for row in column:
-            matrix[y, x] = choose_temperature(size, temperatures, x, y)
-            y += 1
-        x += 1
-
-    return matrix
-
-def build_final_matrix(matrix, results):
+def build_final_matrix(results):
     results_index = 0
-    final_matrix = np.copy(matrix)
-    size = np.array(results).size
+    size =np.int(np.sqrt(np.array(results).size))
+    final_matrix = np.zeros((size, size), dtype=np.int)
 
     
     if size == 1:
@@ -102,45 +83,14 @@ def build_final_matrix(matrix, results):
         for column in final_matrix:
             y = 0
             for row in column:
-                if results_index < size:
-                    if (final_matrix[x,y] == 0 and x > 0 and y > 0 and x < np.array(column).size-1 and y < np.array(column).size-1):
-                        final_matrix[x, y] = results[results_index]
-                        results_index += 1
+                if y < size:
+                    final_matrix[x, y] = results[results_index]
+                    results_index += 1
                     
-                    y += 1
+                y += 1
             x += 1
 
 
     return final_matrix
 
-    
-def choose_temperature(size, temperatures, x, y):
-    if x == 0 and y == 0:
-        return (temperatures["top"] + temperatures["left"]) / 2
-
-    if x == size - 1 and y == size - 1:
-        return (temperatures["right"] + temperatures["bottom"]) / 2
-
-    if x == size - 1 and y == 0:
-        return (temperatures["right"] + temperatures["top"]) / 2
-
-    if x == 0 and y == size - 1:
-        return (temperatures["left"] + temperatures["bottom"]) / 2
-
-    if x == 0:
-        return temperatures["left"]
-
-    if x == size - 1:
-        return temperatures["right"]
-
-    if y == 0:
-        return temperatures["top"]
-
-    if y == size - 1:
-        return temperatures["bottom"]
-
-    return 0
-    
-    
-    
     
