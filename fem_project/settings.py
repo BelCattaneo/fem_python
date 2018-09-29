6b2d4b750e0ce2b5a938987f580bd6d0993cdc26
belen <belcattaneo@gmail.com>
2018-09-29 19:34:30 -0300

    Changed requirements and settings

--- fem_project/settings.py
+++ fem_project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -25,7 +24,7 @@ SECRET_KEY = ')99h=a=*xhgfbzk&i36e6x%bcrn9$bo5^@7kg&6764*kxwr-0r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fem-python.herokuapp.com']
+ALLOWED_HOSTS = ['fem-python.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -122,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "fem\static")
+STATIC_ROOT = os.path.join(BASE_DIR, "fem/static")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "node_modules"),
