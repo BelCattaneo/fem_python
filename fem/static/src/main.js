01fac104bb7eba6c5da25fb07071304a7eb978f1
belen <belcattaneo@gmail.com>
2018-09-30 12:55:44 -0300

    empty plot div

--- fem/static/src/main.js
+++ fem/static/src/main.js
@@ -61,7 +61,7 @@ $( document ).ready(function() {
           }
 
         } else {
-          $("#temperature-plot").empty().append('<div id="spinner"><i class="fa fa-cog fa-spin" style="font-size:24px"></i></div>')
+          $("#temperature-plot").empty().append('<img src="" id="plot" alt=""> <div id="spinner"><i class="fa fa-cog fa-spin" style="font-size:24px"></i></div>')
           $.ajax({
             url: '/ajax/calculate_temperatures/',
             data: {
